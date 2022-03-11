b911cec45002656cc5630e15b68ae0e4baa51e0a
Chukwuka Akibor <juniorakibor@yahoo.com>
2022-03-11 21:12:59 +0100

    adding interactivity to the button hit me

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    
     var body: some View {
         VStack {
-                   Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                   Text("89")
-                   HStack {
-                       Text("1")
-                       Slider(value: .constant(50), in: 1.0...100.0)
-                       Text("100")
-                   }
-                   Button(action: {}) {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .bold()
+            }
+            Button(action: {
+                print("Hello Mena")
+                self.alertIsVisible = true
+            }) {
                Text("Hit Me")
-                   }
-               }
+            }.alert("Hello there!", isPresented: $alertIsVisible) {
+                Button("Awesome!") {}
+            } message: {
+                Text("This is my first pop-up")
+            }
+        }
     }
 }
 
