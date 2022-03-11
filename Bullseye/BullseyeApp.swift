5cdfe7d6e419c93782ba193aa1e9ae6d43560b30
Chukwuka Akibor <cakibor33@gmail.com>
2022-03-11 10:49:37 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Chukwuka Akibor on 11.03.22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
